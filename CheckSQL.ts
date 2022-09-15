37cafb3bbd59068ac32dbeb8f5d420abf2bafa9d
PiemanJ <65600090+PiemanJ@users.noreply.github.com>
2022-09-15 14:50:13 +0200

    start

--- CheckSQL.ts
+++ CheckSQL.ts
@@ -1,1 +1,21 @@
 // Check Sql file for standards compliance.
+import * as fs from 'fs';
+
+
+// Open the file (passed in as param)
+let filename = 'something.Sql';
+let data = fs.readFileSync(filename,'utf8');
+
+// split the data into an array of lines
+
+// parse the lines
+let line = '';
+while (line != null) {
+
+let inComment = false;
+// if line is a comment, ignore it
+
+
+}
+
+
