ced4e2faf4d2d597af0e60b14477da8024c67bc9
PiemanJ <65600090+PiemanJ@users.noreply.github.com>
2022-09-15 18:46:48 +0200

    add stuff

--- CheckSQL.ts
+++ CheckSQL.ts
@@ -9,11 +9,47 @@ let data = fs.readFileSync(filename,'utf8');
 // split the data into an array of lines
 
 // parse the lines
+let inComment = false;
 let line = '';
 while (line != null) {
 
-let inComment = false;
-// if line is a comment, ignore it
+    if(inComment){
+        if (line.contains('*/')) {
+            inComment = false;
+
+            // Strip of the comment part
+            line = line.substring(line.indexOf('*/') + 2).trim();
+        }
+    }
+
+    if(!inComment){
+
+        // if line is a comment, ignore it
+        if (line.trimStart().startsWith('--')) {
+            // ignore line
+        }
+        else if (line.trimStart().startsWith('/*')) {
+            inComment = true;
+        }
+        else {
+            // check it!
+
+            // [] around object names
+
+            // [] around data types
+
+            // ; at EOL
+
+            // Keywords upper: SELECT INSERT DELETE etc
+
+
+            // Table defs have PK / UCI
+
+            
+
+
+        }
+    }
 
 
 }
